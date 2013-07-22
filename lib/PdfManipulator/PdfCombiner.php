b41bbfaa0e2ac532acca99edf74e5d7811427195
thomas <thomas@icontroller.eu>
2013-07-22 10:04:57 +0200

    Add background pdf to first page

--- lib/PdfManipulator/PdfCombiner.php
+++ lib/PdfManipulator/PdfCombiner.php
@@ -41,6 +41,56 @@ class PdfCombiner
 	}
 
 	/**
+	 * @param string Original PDF contents
+	 * @param string Background PDF contents
+	 * @return string Resulting PDF contents
+	 */
+	public function addBackgroundToFirstPage($original, $background)
+	{
+		$sourceFile = tempnam(sys_get_temp_dir(), 'pdf');
+		file_put_contents($sourceFile, $original);
+
+		// if source file has just 1 page
+		$execOutput = array();
+		$commandCheck = "{$this->pdftkBinary} $sourceFile dump_data output | grep -i Num";
+		exec($commandCheck, $execOutput);
+
+		if(trim(substr($execOutput[0], strpos($execOutput[0], ":") + 1)) == 1) {
+			return $this->addBackground($original, $background);
+		}
+
+		// create files
+		$firstFile = tempnam(sys_get_temp_dir(), 'pdf');
+		$catFile = tempnam(sys_get_temp_dir(), 'pdf');
+		$outputFile = tempnam(sys_get_temp_dir(), 'pdf');
+
+		// Extract first page
+		$commandFirstPage = "{$this->pdftkBinary} $sourceFile cat 1 output $firstFile";
+		exec($commandFirstPage, $execOutput);
+
+		// Extract other pages
+		$commandCatPage = "{$this->pdftkBinary} $sourceFile cat 2-end output $catFile";
+		exec($commandCatPage, $execOutput);
+
+		// add background to first page
+		$outputContent = $this->addBackground(file_get_contents($firstFile), $background);
+
+		// combine first page with background and other pages
+		$result = $this->catenate(array($outputContent, file_get_contents($catFile)));
+
+		if(!$result) {
+			throw new \RuntimeException("Couldn't add background to PDF: ".PHP_EOL.$command.PHP_EOL.implode(PHP_EOL, $execOutput));
+		}
+
+		@unlink($sourceFile);
+		@unlink($firstFile);
+		@unlink($catFile);
+		@unlink($outputFile);
+
+		return $result;
+	}
+
+	/**
 	 * @param array List of original PDF contents
 	 * @return string Resulting PDF contents
 	 */
